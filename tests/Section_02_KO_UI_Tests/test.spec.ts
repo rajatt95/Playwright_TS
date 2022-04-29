80f3e2ff8b2ae2e4a582c2ad04059d13da260b73
“rajatt95” <rajatvermaa95@gmail.com>
2022-04-30 00:17:53 +0530

    First commit for UDEMY course - KO_Playwright_TS

---
+++ tests/Section_02_KO_UI_Tests/test.spec.ts
@@ -0,0 +1,17 @@
+/**************************************************/
+
+/**  
+* @author Rajat Verma
+* https://www.linkedin.com/in/rajat-v-3b0685128/
+* https://github.com/rajatt95
+* https://rajatt95.github.io/ 
+*  
+* Course: Automated Software Testing with Playwright (https://www.udemy.com/course/automated-software-testing-with-playwright/)
+* Tutor: Kaniel Outis (https://www.udemy.com/user/shinoku911/)
+*/
+
+/**************************************************/
+
+import {test,expect} from '@playwright/test'
+
+//test('Section_02_KO_UI_Tests')
