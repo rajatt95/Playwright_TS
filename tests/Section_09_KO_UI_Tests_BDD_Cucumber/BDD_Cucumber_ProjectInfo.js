b4da35086e17a0089fafaae1fb9eab09398e1953
“rajatt95” <rajatvermaa95@gmail.com>
2022-05-04 00:03:58 +0530

    Playwright_TS_BDD (Cucumber) details updated

---
+++ tests/Section_09_KO_UI_Tests_BDD_Cucumber/BDD_Cucumber_ProjectInfo.js
@@ -0,0 +1,19 @@
+/**************************************************/
+
+/**  
+* @author Rajat Verma
+* https://www.linkedin.com/in/rajat-v-3b0685128/
+* https://github.com/rajatt95
+* https://rajatt95.github.io/ 
+*  
+* Course: Automated Software Testing with Playwright (https://www.udemy.com/course/automated-software-testing-with-playwright/)
+* Tutor: Kaniel Outis (https://www.udemy.com/user/shinoku911/)
+*/
+
+/**************************************************/
+
+/**
+ * Project name: Playwright_TS_BDD 
+ * https://github.com/rajatt95/Playwright_TS_BDD
+ * About: Automation Testing | Web | Cypress | Typescript | VS Code | Mocha | Cucumber | Cucumber-HTML-Reports
+ */
