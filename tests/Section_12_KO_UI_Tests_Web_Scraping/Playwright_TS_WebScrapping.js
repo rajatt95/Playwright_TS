0633b8914eae2dfcd30b1fbd1e5bac57bc1cb92b
“rajatt95” <rajatvermaa95@gmail.com>
2022-05-04 22:43:55 +0530

    Playwright_TS_WebScrapping added

---
+++ tests/Section_12_KO_UI_Tests_Web_Scraping/Playwright_TS_WebScrapping.js
@@ -0,0 +1,19 @@
+/**************************************************/
+
+/**  
+* @author Rajat Verma
+* https://www.linkedin.com/in/rajat-v-3b0685128/
+* https://github.com/rajatt95
+* https://rajatt95.github.io/ 
+*  
+* Course: Automated Software Testing with Playwright (https://www.udemy.com/course/automated-software-testing-with-playwright/)
+* Tutor: Kaniel Outis (https://www.udemy.com/user/shinoku911/)
+*/
+
+/**************************************************/
+
+/**
+ * Project name: Playwright_TS_WebScrapping
+ * https://github.com/rajatt95/Playwright_TS_WebScrapping
+ * About: Automation Testing | Web | Playwright | Typescript | VS Code | Get Data | Set Data in txt file | Viewport | Context | Web Scrapping
+ */
